1de32bf69b4a21bf4357003fde5234a07eedf353
Lucas Vinicius <88403508+Lucasmcz@users.noreply.github.com>
2021-09-13 11:00:56 -0300

    Create Algoritmofordfulkerson.c
    Algoritmo ainda está sendo feito

---
+++ ford-fulkerson/Algoritmofordfulkerson.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+/*Ler o Arquivo*/
+
+void ler_arq()
+{
+  int vertices;
+  int arestas;
+  scanf("%d %d",&vertices , &arestas);
+}
+
+
+int main()
+{
+  printf("Algoritmo em Execução");
+  return 0;
+}
