36450bf351f7323087b5c7262ffab73c01b8b7fa
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2021-09-15 20:59:53 -0300

    Update Floyd-Warshall.cpp

--- Floyd-Warshall/Floyd-Warshall.cpp
+++ Floyd-Warshall/Floyd-Warshall.cpp
@@ -98,14 +98,14 @@ int main()
 
 /* input:
 6 8
-0 1 5
-0 2 4
-0 3 2
-0 5 6
-1 3 1
-1 4 7
-2 4 6
-3 5 1
+1 2 5
+1 3 4
+1 4 2
+1 6 6
+2 4 1
+2 5 7
+3 5 6
+4 6 1
 
 output:
 A matrix de distancia para o F-W
